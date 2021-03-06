afc0524584efa9acef135ac142a27496732e96c0
Rebee27 <gogrebeca12@gmail.com>
2021-03-06 13:13:16 +0200

    add

--- skills-page/my-skills.js
+++ skills-page/my-skills.js
@@ -1,3 +1,38 @@
+var i = 0;
+var txt = "Creativity";
+var txt2 = " Teamwork Skills"
+var txt3 = " Communication"
+var txt4 = " Adaptability"
+var txt5 = " Empathy"
+var txt6 = " Leadership Skills"
+var txt7 = " Organization"
+var txt8 = " Problem Solving"
+
+var txt9 = " HTML"
+var txt10 = " CSS"
+var txt11 = " Javascript"
+
+var speed = 80;
+
+function typeWriter() {
+    if (i < txt6.length) {
+        document.getElementById("1").innerHTML += txt.charAt(i);
+        document.getElementById("2").innerHTML += txt2.charAt(i);
+        document.getElementById("3").innerHTML += txt3.charAt(i);
+        document.getElementById("4").innerHTML += txt4.charAt(i);
+        document.getElementById("5").innerHTML += txt5.charAt(i);
+        document.getElementById("6").innerHTML += txt6.charAt(i);
+        document.getElementById("7").innerHTML += txt7.charAt(i);
+        document.getElementById("8").innerHTML += txt8.charAt(i);
+
+        document.getElementById("html").innerHTML += txt9.charAt(i);
+        document.getElementById("css").innerHTML += txt10.charAt(i);
+        document.getElementById("javascript").innerHTML += txt11.charAt(i);
+
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
 
 
 
